a359282be082da729350dc969c2dc2e41532dd35
cernevv <laura.cernev24@gmail.com>
2023-08-04 15:13:54 -0300

    exemplos js

---
+++ exemplos/2_TIPOS_DE_DADOS/1_number/script.js
@@ -0,0 +1,9 @@
+console.log(5);
+console.log(37.5);
+console.log(-76)
+console.log(1000.45)
+
+console.log(typeof 5);
+console.log(typeof 37.5);
+console.log(typeof -76)
+console.log(typeof 1000.45)
---
+++ exemplos/2_TIPOS_DE_DADOS/4_string/script.js
@@ -0,0 +1,9 @@
+console.log("Meu nome é Laura.");
+console.log('Desenvolvimento é no Senai');
+
+// Template literals (crase) ao lado da tecla P
+// Usar Shift
+console.log('teste');
+
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
