6f7d8f2c7feedaab76c90a6ae545f668d6c106b3
cernevv <laura.cernev24@gmail.com>
2023-09-21 12:56:52 -0300

    up

--- exemplos/Exercícios JS/1_exercicio_JS_(Sintaxe)/scripts.js
+++ exemplos/Exercícios JS/1_exercicio_JS_(Sintaxe)/scripts.js
@@ -2,3 +2,11 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 
 // Digite a resolução do exercício aqui e visualize o resultado no navegador
+
+let dados = {
+    nascimento: 2006,
+};
+
+document.write("Meu nome é Laura")
+document.write('<h2>Eu moro no Brasil</h2>')
+document.write(`Nasci no ano de: <strong>${dados.nascimento}</strong>`)
--- exemplos/Exercícios JS/2_exercicio_JS_(Comparação)/scripts.js
+++ exemplos/Exercícios JS/2_exercicio_JS_(Comparação)/scripts.js
@@ -2,12 +2,15 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 
 document.write('1º Exemplo: <br><br>');
-
-// Digite a resolução do 1º exemplo aqui e visualize o resultado no navegador
+document.write('3 == 3 && 4 > 1');
+document.write('<br> Resultado: ');
+document.write(3 == 3 && 4 > 1);
 
 document.write('<br><br>');
 document.write('__________________________________________________________<br><br>');
 
 document.write('2º Exemplo: <br><br>');
-
-// Digite a resolução do 2º exemplo aqui e visualize o resultado no navegador
+document.write('4 <= 2 || "Sérgio Pérez" == "Checo"');
+document.write('<br> Resultado:');
+document.write('2º Exemplo: <br><br>');
+document.write('4 <= 2 || "Sérgio Pérez" == "Checo"');
