30eb7317e5fbe4bbb68982678bc56646454b182e
cernevv <laura.cernev24@gmail.com>
2023-09-28 10:42:43 -0300

    up

--- exemplos/Exercícios JS/3_exercicio_JS_(Array bidirecional)/scripts.js
+++ exemplos/Exercícios JS/3_exercicio_JS_(Array bidirecional)/scripts.js
@@ -4,3 +4,11 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 // Digite a resolução do exercício aqui e visualize o resultado no navegador
 
+document.write('<h2 style="color: purple;">Pessoas mais legais da sala 1DE:</h2><br>');
+
+let equipes = [
+    // 0           1             2
+    ['Laura', ' Luana', ' Kaue', ' Gabriela', ' Carol', ' Mateus'], // 0
+];
+
+document.write(`Pessoas mais legais da sala 1DE: <strong>${equipes}</strong>`)
