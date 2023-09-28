5ac98ee4393fcb87253007c467fc41d63a3a6e5c
cernevv <laura.cernev24@gmail.com>
2023-09-28 11:15:45 -0300

    up

--- exemplos/Exercícios JS/4_exercicio_JS_(Array e Loop)/scripts.js
+++ exemplos/Exercícios JS/4_exercicio_JS_(Array e Loop)/scripts.js
@@ -2,7 +2,15 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 
 
-// Digite a resolução do exercício aqui e visualize o resultado no navegador
+let topActors = [
+    "Chris Evans", "Leonardo DiCaprio", "Scarlett Johansson", "Robert Downey Jr", "Lidsay Morgan", "Robert Pattison"
+];
+
+let tamanho = topActors.length;
+
+for (let i = 0; i < tamanho; i ++) {
+    document.write(`<strong>${topActors[i]}</strong><br>`)
+}
 
 
 
