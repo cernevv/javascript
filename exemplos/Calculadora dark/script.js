1e6280303378d8a17385c47a83b02c62852eb603
cernevv <laura.cernev24@gmail.com>
2023-11-16 11:26:13 -0300

    up

--- exemplos/Calculadora dark/script.js
+++ exemplos/Calculadora dark/script.js
@@ -1,30 +1,30 @@
-// Selecione o elemento HTML com o id 'inputBox' e armazena-o na variável 'input'.
+/* Digite o JS aqui */
 let input = document.getElementById('inputBox');
 
-
-// Seleciona todos os elementos HTML do tipo 'button' e armazena-os em 'buttons'.
 let buttons = document.querySelectorAll('button');
 
-// Inicializa uma string vazia para armazenar a entreda do usuário.
 let string = "";
 
-// Converte a coleção de botões em um array para que possamos percorrer sobre eles.
 let arr = Array.from(buttons);
 
-// Percorre sobre cada botão no array 'arr'.
 arr.forEach(button => {
-
-    // Adiciona um ouvinte de eventos de clique a cada botão.
-    button.addEventListener("click", (e) => {
-        // Verifica se o texto dentro do botão clicado é '='.
+    button.addEventListener("click", (e)=> {
         if (e.target.innerHTML == '=') {
-
-            // A função eval() é uma função JavaScript que avalia e executa dinamicamente o código. Quando a linha é executada, o JavaScript interpreta o conteúdo de string como uma expressão e a avalia. No exemplo dado, eval("2 + 2") resultaria em 4, porque ele calcula a soma.
             string = eval(string);
 
-            // Define o valor da caixa de entrada 'input' como a nova string calculada.
+            input.value = string;
+        } 
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
             input.value = string;
         }
-    }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.lenght - 1);
+            input.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
 })
-
